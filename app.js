1dccc0eeadedec24a16b36d1937e74d165463e2c
Ben-bot-Platform <bibimoshfiqakarwan@gmail.com>
2024-12-21 16:39:05 +0430

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const app = require('./index.js'); // ارجاع به فایل اصلی پروژه
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 module.exports = app;
