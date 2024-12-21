8f751c63e398e50c157761196c1ebb8c5f1d3d51
Ben-bot-Platform <bibimoshfiqakarwan@gmail.com>
2024-12-21 17:06:52 +0430

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const app = require('./index.js'); // ارجاع به فایل اصلی پروژه
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-module.exports = app;
